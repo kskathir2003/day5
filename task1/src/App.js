5a995f0e446189e8ae6644960c5acf4c9bab0a26
kathirks2003 <727721euec067@skcet.ac.in>
2023-04-05 15:38:28 +0530

    first commit

---
+++ task1/src/App.js
@@ -0,0 +1,18 @@
+/* eslint-disable */
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ErrorBoundary from './errorhandler';
+
+class App extends Component {
+  render() {
+    return (
+     
+     <div>
+      <ErrorComponents/>
+     </div>
+    );
+  }
+}
+
+export default App;
